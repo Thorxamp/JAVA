a6751ec48b68395342cd0aec65df9196e7560647
Amrit A Kalmane <129938811+Thorxamp@users.noreply.github.com>
2023-08-02 17:58:35 +0530

    Add files via upload

---
+++ gender.java
@@ -0,0 +1,14 @@
+//pro to print sim msg.
+class gender 
+{
+  public static void main(String[]args)
+   {
+    char ch='m';
+    if(ch=='f') 
+    System.out.println("Female");
+    else if(ch=='m') 
+    System.out.println("Male");
+    else
+    System.out.println("Others");
+   }
+}
